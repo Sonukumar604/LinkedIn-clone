028421b76996aec13566cbfa3a80abad52593ac2
Sonu Kumar <sonusaini48292@gmail.com>
2026-08-27 15:30:55 +0530

    fix(api-gateway): update microservice routes and actuator config in application.yml

--- connectionsService/src/main/java/com/LinkedInProject/connectionsService/ConnectionsServiceApplication.java
+++ connectionsService/src/main/java/com/LinkedInProject/connectionsService/ConnectionsServiceApplication.java
@@ -2,12 +2,15 @@ package com.LinkedInProject.connectionsService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class   ConnectionsServiceApplication {
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ConnectionsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConnectionsServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectionsServiceApplication.class, args);
+    }
 }
